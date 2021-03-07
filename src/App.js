2052fc30cb41e3872d3ea72cdfc9c38cbe1db329
claytonNishitaniHart <claytonnishitanihart@gmail.com>
2021-03-08 05:22:25 +1300

    drop box for uploading

--- src/App.js
+++ src/App.js
@@ -5,18 +5,37 @@ import { app } from './base';
 function App() {
   const [fileLink, setFileLink] = useState(null);
 
-  const handleChange = async (e) => {
+  const handleChange = (e) => {
     const file = e.target.files[0];
+    uploadImage(file);
+  };
+
+  const handleDrag = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const file = e.dataTransfer.files[0];
+    uploadImage(file);
+  }
+
+  const uploadImage = async (file) => {
     const storageRef = app.storage().ref();
     const fileRef = storageRef.child(file.name);
     await fileRef.put(file);
     setFileLink(await fileRef.getDownloadURL());
-  };
+  }
 
-  const image = fileLink ? <img src={fileLink} alt='upload' /> : '';
+  const image = fileLink ? <img className='preview' src={fileLink} alt='upload' /> : '';
 
   return (
     <div className="App">
+      <div className='dropbox' onDragEnter={handleDrag} onDragOver={handleDrag} onDragExit={handleDrag} onDrop={handleDrop}>
+        <p>drop here</p>
+      </div>
       <form>
         <label>
           Input:
