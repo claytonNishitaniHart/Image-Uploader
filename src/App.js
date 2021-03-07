bd46ac54508cccf88f3eeba19bdfba6cdaec41b7
claytonNishitaniHart <claytonnishitanihart@gmail.com>
2021-03-08 06:13:11 +1300

    separated uploading and viewing parts

--- src/App.js
+++ src/App.js
@@ -1,49 +1,27 @@
 import './App.css';
 import React, { useState } from 'react';
-import { app } from './base';
+import Ready from './READY';
 
 function App() {
+  const [situation, setSituation] = useState('ready');
   const [fileLink, setFileLink] = useState(null);
+  let display = <div></div>
 
-  const handleChange = (e) => {
-    const file = e.target.files[0];
-    uploadImage(file);
-  };
-
-  const handleDrag = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
+  switch (situation) {
+    case 'ready':
+      display = <Ready setSituation={setSituation} setFileLink={setFileLink} />
+      break;
+    case 'done':
+      display = <div>{fileLink}</div>
+      break;
+    default:
+      display = <Ready setSituation={setSituation} setFileLink={setFileLink} />
+      break;
   }
-
-  const handleDrop = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    const file = e.dataTransfer.files[0];
-    uploadImage(file);
-  }
-
-  const uploadImage = async (file) => {
-    const storageRef = app.storage().ref();
-    const fileRef = storageRef.child(file.name);
-    await fileRef.put(file);
-    setFileLink(await fileRef.getDownloadURL());
-  }
-
-  const image = fileLink ? <img className='preview' src={fileLink} alt='upload' /> : '';
 
   return (
     <div className="App">
-      <div className='dropbox' onDragEnter={handleDrag} onDragOver={handleDrag} onDragExit={handleDrag} onDrop={handleDrop}>
-        <p>drop here</p>
-      </div>
-      <form>
-        <label>
-          Input:
-          <input type='file' id='input' onChange={handleChange} accept='image/*' />
-        </label>
-      </form>
-      {image}
-      <p>{fileLink}</p>
+      {display}
     </div>
   );
 }
---
+++ src/READY.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { app } from './base';
+import './READY.css';
+
+const Ready = ({ setSituation, setFileLink }) => {
+  const [isUploading, setIsUploading] = useState(false);
+
+  const handleChange = (e) => {
+    const file = e.target.files[0];
+    uploadImage(file);
+  };
+
+  const handleDrag = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const file = e.dataTransfer.files[0];
+    uploadImage(file);
+  }
+
+  const uploadImage = async (file) => {
+    setIsUploading(true);
+    const storageRef = app.storage().ref();
+    const fileRef = storageRef.child(file.name);
+    await fileRef.put(file);
+    setFileLink(await fileRef.getDownloadURL());
+    setIsUploading(false);
+    setSituation('done');
+  }
+
+  const uploading = isUploading ? <div>uploading</div> :
+    <>
+      <div className='dropbox' onDragEnter={handleDrag} onDragOver={handleDrag} onDragExit={handleDrag} onDrop={handleDrop}>
+        <p>drop here</p>
+      </div>
+      <form>
+        <label>
+          Input:
+          <input type='file' id='input' onChange={handleChange} accept='image/*' />
+        </label>
+      </form>
+    </>
+
+  return (
+    <>
+      {uploading}
+    </>
+  );
+}
+
+export default Ready;
